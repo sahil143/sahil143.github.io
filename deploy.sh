db4c9a1e148367eda071e938cb2c299c8d4ea2d6
sahil143 <sahilbudhwar143@gmail.com>
2019-03-22 23:26:47 +0530

    fix errors in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-
+yarn run build
 git branch -D master
 git checkout -b master
 
 for x in *
 do
-  if ["$x" != "build"] && ["$x" != "deploy.sh"]
+  if [ "$x" != "build" ] && [ "$x" != "deploy.sh" ]
   then
     rm -rf $x;
   fi
