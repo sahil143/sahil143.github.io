9ce2bdbefcf4a5596c3f8cb9377c728a9b11d8ca
sahil143 <sahilbudhwar143@gmail.com>
2019-03-22 23:01:48 +0530

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+git branch -D master
+git checkout -b master
+
+for x in *
+do
+  if ["$x" != 'build' && "$x" != "deploy.sh"]
+  then
+    rm -rf $x;
+  fi
+done;
+
+cd build
+mv * ../.
+cd ..
+rm -rf build
+git push origin master
