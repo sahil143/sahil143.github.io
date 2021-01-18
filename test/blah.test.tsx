cc69549d0ab43cbd9f6f10d77ebc0e8190aeec37
Sahil Budhwar <sahilbudhwar143@gmail.com>
2021-01-18 15:16:04 +0530

    remove useless code

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../src';
+import { Sahil143GitHubIO } from '../src';
 
 describe('it', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
+    ReactDOM.render(<Sahil143GitHubIO />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
